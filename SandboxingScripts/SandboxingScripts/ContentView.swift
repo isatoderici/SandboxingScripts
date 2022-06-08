99d978db9160d351ae95e695fa928a9c05a6cf39
isatoderici <65185855+isatoderici@users.noreply.github.com>
2022-06-07 17:48:28 -0700

    Initial commit

---
+++ SandboxingScripts/SandboxingScripts/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SandboxingScripts
+//
+//  Created by Isabela Toderici on 6/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SandboxingScripts/SandboxingScripts/SandboxingScriptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxingScriptsApp.swift
+//  SandboxingScripts
+//
+//  Created by Isabela Toderici on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SandboxingScriptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
